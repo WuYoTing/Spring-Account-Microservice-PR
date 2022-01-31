a00d6555e039425e16887dca3796d9f8b5d46fec
Tim <hakosaki314@gmail.com>
2022-01-31 23:50:24 +0800

    feat: initial project

---
+++ src/main/java/com/example/springaccountmicroservicepr/SpringAccountMicroservicePrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaccountmicroservicepr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAccountMicroservicePrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAccountMicroservicePrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springaccountmicroservicepr/SpringAccountMicroservicePrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaccountmicroservicepr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAccountMicroservicePrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
