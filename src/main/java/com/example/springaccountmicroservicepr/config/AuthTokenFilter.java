2dcf6ea8a4792aeab5b0ca49eda0879e161fe3c6
Tim <hakosaki314@gmail.com>
2022-02-05 23:10:27 +0800

    feat: add request filter and jwt util

---
+++ src/main/java/com/example/springaccountmicroservicepr/config/AuthTokenFilter.java
@@ -0,0 +1,48 @@
+package com.example.springaccountmicroservicepr.config;
+
+import com.example.springaccountmicroservicepr.services.impl.UserDetailsServiceImpl;
+import com.example.springaccountmicroservicepr.util.JwtUtils;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Log4j2
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+	private JwtUtils jwtUtils;
+
+	private UserDetailsServiceImpl userDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		try {
+			// get JWT from the Authorization header (by removing Bearer prefix)
+			String jwt = parseJwt(request);
+			if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+				String username = jwtUtils.getUserNameFromJwtToken(jwt);
+				UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+					userDetails, null, userDetails.getAuthorities());
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// set the current UserDetails in SecurityContext using
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		} catch (Exception e) {
+			logger.error("Cannot set user authentication: {}", e);
+		}
+		filterChain.doFilter(request, response);
+	}
+}
