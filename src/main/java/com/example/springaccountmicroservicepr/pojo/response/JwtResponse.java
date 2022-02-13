61c2a5a26532ab9c716a451deb8608598dfcd337
Tim <hakosaki314@gmail.com>
2022-02-13 21:01:01 +0800

    fix: pipeline fail

--- src/main/java/com/example/springaccountmicroservicepr/pojo/response/JwtResponse.java
+++ src/main/java/com/example/springaccountmicroservicepr/pojo/response/JwtResponse.java
@@ -1,13 +1,11 @@
 package com.example.springaccountmicroservicepr.pojo.response;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class JwtResponse {
 
 	private String type = "Bearer";
@@ -17,4 +15,14 @@ public class JwtResponse {
 	private String username;
 	private String email;
 	private List<String> roles;
+
+	public JwtResponse(String token, String refreshToken, Long id, String username,
+		String email, List<String> roles) {
+		this.token = token;
+		this.refreshToken = refreshToken;
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.roles = roles;
+	}
 }
