272e9867e86a36d97674817a645a43b641e85a53
Tim <hakosaki314@gmail.com>
2022-04-11 23:07:38 +0800

    feat: add database schema and data

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,3 +1,15 @@
-INSERT INTO roles_type(name) VALUES('ROLE_USER');
-INSERT INTO roles_type(name) VALUES('ROLE_MODERATOR');
-INSERT INTO roles_type(name) VALUES('ROLE_ADMIN');
+-- roles_type
+INSERT INTO public.roles_type(name)
+VALUES ('ROLE_USER');
+INSERT INTO public.roles_type(name)
+VALUES ('ROLE_MODERATOR');
+INSERT INTO public.roles_type(name)
+VALUES ('ROLE_ADMIN');
+-- users
+INSERT INTO public.users
+  (id, username, email, "password", refresh_token_id)
+VALUES (1, 'test', 'test@gmail.com', 'test', NULL);
+-- user_roles
+INSERT INTO public.user_roles
+  (user_id, role_id)
+VALUES (1, 1);
