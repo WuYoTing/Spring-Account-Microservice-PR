5e23d1363f34549ee1d3cfd1340eeb11d10f1fe8
Tim <hakosaki314@gmail.com>
2022-04-12 22:03:57 +0800

    fix: pipeline fail

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -7,5 +7,9 @@ INSERT INTO public.roles_type(name)
 VALUES ('ROLE_ADMIN');
 -- users
 INSERT INTO public.users
-  (username, email, "password", refresh_token_id)
-VALUES ('test', 'test@gmail.com', 'test', NULL);
+  (username, email, "password")
+VALUES ('test', 'test@gmail.com', 'test');
+-- user_roles
+INSERT INTO public.user_roles
+  (user_id, role_id)
+VALUES (1, 1);
