25d40f17b0fb4bac70309f6b35351c197655485e
Tim <hakosaki314@gmail.com>
2022-04-25 22:40:11 +0800

    feat: add password reset entity

--- src/main/java/com/example/springaccountmicroservicepr/pojo/dao/PasswordResetToken.java
+++ src/main/java/com/example/springaccountmicroservicepr/pojo/dao/PasswordResetToken.java
@@ -2,36 +2,27 @@ package com.example.springaccountmicroservicepr.pojo.dao;
 
 import java.time.Instant;
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity(name = "refresh_token")
-public class RefreshToken {
+public class PasswordResetToken {
+
+	private static final int EXPIRATION = 15;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+
 	@OneToOne(targetEntity = User.class)
 	@JoinColumn(name = "user_id", referencedColumnName = "id")
 	private User user;
+
 	@Column(nullable = false, unique = true)
 	private String token;
+
 	@Column(nullable = false)
 	private Instant expiryDate;
-
-	public RefreshToken(User user, String token, Instant expiryDate) {
-		this.user = user;
-		this.token = token;
-		this.expiryDate = expiryDate;
-	}
 }
